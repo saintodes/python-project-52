3876f01914472fc6bb1720e3b3633b64c68cfa33
Egor E udk <odes36@gmail.com>
2024-02-21 13:44:21 +0300

    user list done

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.db.models import Value
+from django.db.models.functions import Concat
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from users.forms import RegisterUserForm
 
@@ -11,3 +14,12 @@ class RegisterUser(CreateView):
     template_name = 'users/create.html'
     extra_context = {'title': "Registration"}
     success_url = reverse_lazy('login')
+
+
+class UsersView(ListView):
+    template_name = 'users/users_list.html'
+    context_object_name = 'users_list'
+    title_page = 'Users'
+
+    def get_queryset(self):
+        return User.objects.only('id', 'username', 'first_name', 'last_name', 'date_joined')
