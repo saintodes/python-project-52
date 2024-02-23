a0bb726e375b3ff5d494ff0adba1517fa411634d
Egor E udk <odes36@gmail.com>
2024-02-23 11:48:16 +0300

    add translate and flash messages

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -1,2 +1,6 @@
-menu = {'title': 'Main_page', 'url_name': 'home',
-        }
+from django.utils.translation import gettext
+
+FLASH_MESSAGES_TEXT = {
+    "login_success": gettext('You are logged in'),
+    'logout_success': gettext('You are logged out'),
+}
--- users/forms.py
+++ users/forms.py
@@ -3,24 +3,25 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 import re
+from django.utils.translation import gettext
 
 
 class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label="Login", widget=forms.TextInput(attrs={'class': 'form-control'}),
-                               help_text="Required field. No more than 150 characters. Only letters, numbers, and symbols. @/./+/-/_.")
+                               help_text=gettext("Required field. No more than 150 characters. Only letters, numbers, and symbols. @/./+/-/_."))
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(
         attrs={'class': 'form-control', "autocomplete": "new-password"}),
-                                help_text='Your password must contain at least 3 characters.')
+                                help_text=gettext('Your password must contain at least 3 characters.'))
     password2 = forms.CharField(label="Password repeat", widget=forms.PasswordInput(
         attrs={'class': 'form-control', "autocomplete": "new-password"}),
-                                help_text='Please enter your password again for confirmation.')
+                                help_text=gettext('Please enter your password again for confirmation.'))
 
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
         labels = {
-            'first_name': "First name",
-            'last_name': "Last name",
+            'first_name': gettext("First name"),
+            'last_name': gettext("Last name"),
         }
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
@@ -30,13 +31,13 @@ class RegisterUserForm(UserCreationForm):
     def clean_password1(self):
         password = self.cleaned_data.get('password1')
         if len(password) < 3:
-            raise ValidationError("Your password must contain at least 3 characters.")
+            raise ValidationError(gettext("Your password must contain at least 3 characters."))
         return password
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if len(username) > 150:
-            raise ValidationError("The username length cannot exceed 150 characters.")
+            raise ValidationError(gettext("The username length cannot exceed 150 characters."))
         if not re.match(r'^[\w.@+-]+$', username):
-            raise ValidationError("The username can only contain letters, numbers, and the symbols @/./+/-/_.")
+            raise ValidationError(gettext("The username can only contain letters, numbers, and the symbols @/./+/-/_."))
         return username
