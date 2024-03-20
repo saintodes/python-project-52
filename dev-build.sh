fbe28684de640e17f1ae78e321accc2216609e4b
Egor E udk <odes36@gmail.com>
2024-03-20 11:30:38 +0300

    Added postgresql db

--- dev-build.sh
+++ dev-build.sh
@@ -2,22 +2,15 @@
 
 #Build this .sh to deploy app and db at localhost
 
+# Attempt to get DATABASE_URL from .env file
+DATABASE_URL=$(grep -E "^DATABASE_URL=" ./.env | cut -d '=' -f2-)
 
-# Source .env file to get environment variables
-if [[ -f ./.env ]]; then
-    source ./.env
-else
-    echo "Error: .env file not found in the root directory."
+if [[ -z "$DATABASE_URL" ]]; then
+    echo "Error: .env file is missing or DATABASE_URL environment variable is not set."
     exit 1
 fi
 
-# Check if DATABASE_URL is set
-if [ -z "$DATABASE_URL" ]; then
-    echo "Error: DATABASE_URL environment variable is not set."
-    exit 1
-else
-    echo "Using DATABASE_URL: $DATABASE_URL"
-fi
+echo "Using DATABASE_URL: $DATABASE_URL"
 
 # Parse db url
 regex="postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.+)"
@@ -43,6 +36,21 @@ if sudo service postgresql start; then
     echo "PostgreSQL service started succesfully"
 else
     echo "Error: Failed to start PostgreSQL service"
+fi
+
+#Check if db role exists
+ROLE_EXISTS=$(sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER'")
+if [ "$ROLE_EXISTS" = "1" ]; then
+    echo "Database role '$DB_USER' already exists. Skipping creation."
+else
+    # Create db role
+    COMMAND_CREATE_ROLE="CREATE ROLE $DB_USER WITH CREATEDB LOGIN PASSWORD '$DB_USER_PASSWORD';"
+    if sudo -u postgres psql -c "$COMMAND_CREATE_ROLE"; then
+        echo "Database role '$DB_USER' created successfully."
+    else
+        echo "Error: Failed to create database role '$DB_USER'."
+        exit 1
+    fi
 fi
 
 
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,12 +94,17 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
