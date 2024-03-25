5c59d232e26d9ad68462ef6d09c5a89cf01b8b5c
odes <odes36@gmail.com>
2024-03-25 17:01:23 +0300

    Translations fixes

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,26 +1,26 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
 
 
 class Tasks(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Name', unique=True)
-    description = models.CharField(max_length=150, verbose_name='Description', null=True)
+    name = models.CharField(max_length=150, verbose_name=_('Name'), unique=True)
+    description = models.CharField(max_length=150, verbose_name=_('Description'), null=True)
 
-    status = models.ForeignKey('statuses.Status', on_delete=models.PROTECT, null=True, verbose_name='Status',
+    status = models.ForeignKey('statuses.Status', on_delete=models.PROTECT, null=True, verbose_name=_('Status'),
                                db_index=True)
-    label = models.ManyToManyField('labels.Labels', blank=True, verbose_name=gettext('Label'),
+    label = models.ManyToManyField('labels.Labels', blank=True, verbose_name=_('Label'),
                                    related_name='task_labels')
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Creation Time')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation Time'))
     time_update = models.DateTimeField(auto_now=True, verbose_name='Update Time')
     author_id = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='author',
-                                  null=True, blank=True, verbose_name="Author", db_index=True)
+                                  null=True, blank=True, verbose_name=_("Author"), db_index=True)
     executor_id = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='performer',
-                                    null=True, blank=True, verbose_name="Executor", db_index=True)
+                                    null=True, blank=True, verbose_name=_("Executor"), db_index=True)
 
     class Meta:
-        verbose_name = gettext('Task')
-        verbose_name_plural = gettext('Tasks')
+        verbose_name = _('Task')
+        verbose_name_plural = _('Tasks')
 
     def __str__(self):
         return self.name
