edd4dddf449fefee397a45ca5b891146e046fe7d
saintodes <odes36@gmail.com>
2024-03-28 09:55:05 +0300

    Renamed Tasks model fields

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -5,9 +5,9 @@ from task_manager.tasks.models import Tasks
 # Register your models here.
 @admin.register(Tasks)
 class TasksAdmin(admin.ModelAdmin):
-    fields = ['name', 'author_id', 'executor_id', 'status', 'label', 'description', 'time_create',
+    fields = ['name', 'author', 'executor', 'status', 'label', 'description', 'time_create',
               'time_update']
     readonly_fields = ['time_create', 'time_update']
-    list_display = ['name', 'author_id', 'executor_id', 'status', 'description', 'time_create',
+    list_display = ['name', 'author', 'executor', 'status', 'description', 'time_create',
                     'time_update']
-    list_display_links = ['name', 'executor_id']
+    list_display_links = ['name', 'executor']
