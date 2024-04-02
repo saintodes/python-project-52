01cff89c331b1709314e2e95e027a0ca75c3e34a
Egor E udk <odes36@gmail.com>
2024-04-02 08:51:57 +0300

    Used get_full_name() method from AbstractUser model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.get_full_name()
 
     def __str__(self):
         return self.full_name
